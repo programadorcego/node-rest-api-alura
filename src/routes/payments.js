6659d0fd5b2d1656f47d3995075fed530f8dcba1
Programador Cego <programadorcego@gmail.com>
2023-11-04 17:50:32 -0300

    2.5: Aplicando validações na entrada

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -1,23 +1,40 @@
 const payments = require('express').Router();
+const { json } = require('express');
+const { check, validationResult } = require('express-validator');
 
 const PaymentModel = require('../models/Payment');
 const Payment = new PaymentModel();
 
-payments.use(require('express').json());
+payments.use(json());
 
 payments.get('/payments', (req, res) => {
     res.send('Pagamentos');
 });
 
-payments.post('/payments/payment', (req, res) => {
-    let payment = req.body;
+payments.post('/payments/payment', [
+    check('payment').notEmpty().withMessage('payment is required'),
+    check('price').notEmpty().isFloat().withMessage('price is required and must be decimal')
+], (req, res) => {
+    console.log('Processing new payment request');
+
+    const payment = req.body;
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        console.log('Validation Errors');
+        return res.status(400).json({errors: errors.array()});
+    }
     payment.status = 'created';
     payment.date = new Date();
     
     Payment.save(payment, (err, result) => {
-        if(err) return console.log(err.message);
-        console.log('Pagamento Criado');
-        console.log(result);
+        if(err) {
+            res.status(400);
+            console.log(`Error inserting into database: ${err}`);
+            return res.send(err);
+        }
+        console.log('Payment Created');
         res.json(payment);
     });
 });
