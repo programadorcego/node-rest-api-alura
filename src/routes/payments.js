b11994561f984cac06296c557d3b9d8704584885
Programador Cego <programadorcego@gmail.com>
2023-11-04 12:42:39 -0300

    2.1: Criando rota para criar pagamento

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -6,4 +6,10 @@ payments.get('/payments', (req, res) => {
     res.send('Pagamentos');
 });
 
+payments.post('/payments/payment', (req, res) => {
+    let payment = req.body;
+    console.log(JSON.stringify(payment));
+    res.send('ok');
+});
+
 module.exports = payments;
